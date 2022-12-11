d8d6aadb9b62b3833945f02dfba14016da3e0231
spark <pnw123456@icloud.com>
2022-12-11 10:45:39 +0900

    feat: #1 controller layer add

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+
+from src.web import HelloResource
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloResource, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/web/__init__.py
@@ -0,0 +1,1 @@
+from .hello_resource import HelloResource
